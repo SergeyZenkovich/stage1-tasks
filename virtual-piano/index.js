5ecc03da7abdb02069e7ab6506fc7939015d6555
Sergey Zenkovich <sapocalipsys.z@gmail.com>
2021-03-05 00:31:11 +0300

    feat: add all functionality of task

--- virtual-piano/index.js
+++ virtual-piano/index.js
@@ -6,7 +6,6 @@ const fullScreenButton = document.querySelector('.fullscreen');
 
 function addClassToKeys(addedClass) {
     pianoКeys.forEach((el)=>{
-        console.log(el);
         el.classList.add(addedClass)}
         );
 }
@@ -35,7 +34,56 @@ function toggleFullScreen() {
       }
     }
   }
+function audioPlayByKeyboard(e){
+    if(e.repeat) return;
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const note  = document.querySelector(`.piano-key[data-letter="${e.code[e.code.length-1]}"]`);
+    if(!note) return;
+    if(!audio) return;
+    note.classList.add('piano-key-active');
+    audio.currentTime = 0;
+    audio.play();
+}
+function audioPlayByClick(e){
+    if(!e.target.classList.contains('piano-key'))return;
+    pianoКeys.forEach((el) => {
+        if(el.classList.contains('active')) {
+          el.classList.remove('active');
+        }
+      });
+      const audio = document.querySelector(`audio[data-key="Key${e.target.dataset.letter}"]`);
+      e.target.classList.add('active');
+      audio.currentTime = 0;
+      audio.play();
+      glissando();
+}
+function PlayOnOver(e){
+    if(!e.target.classList.contains('piano-key'))return;
+    const audio = document.querySelector(`audio[data-key="Key${e.target.dataset.letter}"]`);
+    audio.play()
+}
+function glissando(){
+    pianoКeys.forEach((el) => {
+          el.addEventListener('mouseover', PlayOnOver);
+      });
+}
+function stopPlayingByClick(){
+    pianoКeys.forEach((el) => {
+        el.removeEventListener('mouseover', PlayOnOver);
+    });
+}
+function endTransform(e){
+    const note  = document.querySelector(`.piano-key[data-letter="${e.code[e.code.length-1]}"]`);
+    if(!note)return; 
+    note.classList.remove('piano-key-active');
+
+}
 
 lettersButton.addEventListener('click', makeLettersActive);
 notesButton.addEventListener('click', makeNotessActive);
 fullScreenButton.addEventListener('click', toggleFullScreen);
+window.addEventListener('keydown', audioPlayByKeyboard);
+window.addEventListener('keyup', endTransform);
+piano.addEventListener('mousedown', audioPlayByClick);
+piano.addEventListener('mouseup', stopPlayingByClick)
+
